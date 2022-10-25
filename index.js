75d264e5e566376b83ee78d062f8f0bfbe719ad5
Felipe Santos <felipecassiors@gmail.com>
2022-10-25 10:38:09 -0300

    fix: prepare for gh actions set-output deprecation

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-function setOutput(name, value, stdout) {
-  stdout.write(`::set-output name=${name}::${value}\n`);
+// @ts-check
+
+const core = require('@actions/core');
+
+function verifyConditions() {
+  core.setOutput("new-release-published", "false");
 }
 
-function verifyConditions(_pluginConfig, { stdout }) {
-  setOutput("new-release-published", "false", stdout);
-}
-
-function generateNotes(pluginConfig, { nextRelease, stdout }) {
-  setOutput("new-release-published", "true", stdout);
-  setOutput("new-release-version", nextRelease.version, stdout);
+function generateNotes(_pluginConfig, { nextRelease }) {
+  core.setOutput("new-release-published", "true");
+  core.setOutput("new-release-version", nextRelease.version);
 }
 
 module.exports = {
