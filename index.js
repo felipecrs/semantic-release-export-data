843e80992e15bd5c47f64b448aa18994f37dd2e7
Carlos Cortón Cobas <104267232+carloscortonc@users.noreply.github.com>
2025-10-05 15:38:38 +0200

    feat: update output delimiter & include `notes` if present

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
 
 const { env } = require("node:process");
 const { appendFileSync } = require("node:fs");
+const { EOL } = require("node:os");
+const { randomUUID } = require("node:crypto");
 
 function exportData(name, value) {
   console.log(`semantic-release-export-data: ${name}=${value}`);
   if (env.GITHUB_OUTPUT) {
-    const output = `${name}=${value}\n`;
+    // Borrowed from https://github.com/actions/toolkit/blob/ddc5fa4ae84a892bfa8431c353db3cf628f1235d/packages/core/src/file-command.ts#L27  
+    const delimiter = "output_delimiter_".concat(randomUUID());
+    const output = "".concat(name, "<<", delimiter, EOL, value, EOL, delimiter, EOL);
     appendFileSync(env.GITHUB_OUTPUT, output);
   }
 }
@@ -19,6 +23,8 @@ function generateNotes(_pluginConfig, { nextRelease }) {
   exportData("new-release-published", "true");
   exportData("new-release-version", nextRelease.version);
   exportData("new-release-git-tag", nextRelease.gitTag);
+  // Export notes if present
+  nextRelease.notes && exportData("new-release-release-notes", nextRelease.notes);
 }
 
 module.exports = {
